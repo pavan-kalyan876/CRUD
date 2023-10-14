9b92915bb469e9845880e53273ea063555bfcf8c
pavansta <pavan.learn69@gmail>
2023-10-15 01:27:52 +0530

    Given data in ui printing data  in terminal

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from django import views
 from app import views
 urlpatterns = [
-  path('',views.index,name="index.html"),
+  path('',views.index,name="index"),
+  path('about',views.about,name="about"),
+  path('insert',views.insertData,name="insertData"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        gender = request.POST.get("gender")
+        print(name,email,age,gender)
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
